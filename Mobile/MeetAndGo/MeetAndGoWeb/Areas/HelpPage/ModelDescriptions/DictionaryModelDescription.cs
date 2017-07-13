f3e80d0d14f943142bcb706da3104c81526b2e2b
lHubertl <lhubertl@outlook.com>
2017-07-13 17:20:14 +0300

    Added Web project and tests

---
+++ Mobile/MeetAndGo/MeetAndGoWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MeetAndGoWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Mobile/MeetAndGo/MeetAndGoWeb/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MeetAndGoWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
