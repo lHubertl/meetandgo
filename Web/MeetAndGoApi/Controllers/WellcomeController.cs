f7b400bc0b936d23082dd3197e4b8a6642a0cb97
Valerii Sovytskyi <valerii.sovytskyi@jetsoftpro.com>
2018-10-18 14:23:39 +0300

    Added wellcome page controller and changed default path

---
+++ Web/MeetAndGoApi/Controllers/WellcomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetAndGoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WellcomeController : ControllerBase
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public string Get()
+        {
+            return "Hello mate";
+        }
+    }
+}
