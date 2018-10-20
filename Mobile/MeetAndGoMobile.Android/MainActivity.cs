5a48ba3e43384d1feb1796494f29eccb999f76de
Valerii Sovytskyi <valerii.sovytskyi@jetsoftpro.com>
2018-10-21 00:54:37 +0300

    Added infrastructure project for mobile

--- Mobile/MeetAndGoMobile.Android/MainActivity.cs
+++ Mobile/MeetAndGoMobile.Android/MainActivity.cs
@@ -6,7 +6,13 @@ using Prism.Ioc;
 
 namespace MeetAndGoMobile.Droid
 {
-    [Activity(Label = "MeetAndGoMobile", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(
+        Label = "Meet & Go", 
+        Icon = "@mipmap/ic_launcher", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
