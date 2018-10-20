da3b4c3bcd4eb6c94cc508016b2b2629d1ac9b79
Valerii Sovytskyi <valerii.sovytskyi@jetsoftpro.com>
2018-10-21 00:44:47 +0300

    Added initial xamarin forms project with UWP, Android, iOS projects

---
+++ Mobile/MeetAndGoMobile.UWP/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prism;
+using Prism.Ioc;
+
+namespace MeetAndGoMobile.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            LoadApplication(new MeetAndGoMobile.App(new UwpInitializer()));
+        }
+    }
+
+    public class UwpInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register any platform specific implementations
+        }
+    }
+}
