29e392cd3701976ab166f46b6220851fd25e81b3
Valerii Sovytskyi <valerii.Sovytskyi@jetsoftpro.com>
2018-11-15 18:17:52 +0200

    Added extended button

---
+++ Mobile/MeetAndGoMobile.Android/Renderers/ExtendedButtonRenderer.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using MeetAndGoMobile.Droid.Renderers;
+using MeetAndGoMobile.Infrastructure.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ExtendedButton), typeof(ExtendedButtonRenderer))]
+
+namespace MeetAndGoMobile.Droid.Renderers
+{
+    public class ExtendedButtonRenderer : ButtonRenderer
+    {
+        public ExtendedButtonRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (!(Element is ExtendedButton element) || Control is null)
+            {
+                return;
+            }
+
+            if (element.ActiveGradient)
+            {
+                ConfigureGradient(element);
+            }
+
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (Element is null || Control is null)
+            {
+                return;
+            }
+
+            if (e.PropertyName == VisualElement.HeightProperty.PropertyName)
+            {
+
+            }
+        }
+
+        private void ConfigureGradient(ExtendedButton element)
+        {
+            //Element.BackgroundColor = Color.Transparent;
+
+            //var startColor = element.GradientStartColor.ToAndroid();
+            //var endColor = element.GradientEndColor.ToAndroid();
+
+            //var mask = new GradientDrawable();
+            //mask.SetColor(Color.Red.ToAndroid());
+            //mask.SetCornerRadius(element.CornerRadius * Resources.DisplayMetrics.Density);
+
+            //var gd = new GradientDrawable(GradientDrawable.Orientation.TlBr, new int[] {startColor, endColor});
+
+            //int[][] states = new int[][] { new int[] { Control.DrawingCacheBackgroundColor } };
+            //int[] colors = new int[] { Color.Blue.ToAndroid() }; // sets the ripple color to blue
+
+            //ColorStateList colorStateList = new ColorStateList(states, colors);
+
+            //var ripple = new RippleDrawable(colorStateList, gd, mask);
+
+            //Control.Background = ripple;
+        }
+    }
+}
---
+++ Mobile/MeetAndGoMobile.Infrastructure/Controls/ExtendedButton.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms;
+
+namespace MeetAndGoMobile.Infrastructure.Controls
+{
+    public class ExtendedButton : Button
+    {
+        public static readonly BindableProperty RoundedHorizontallyProperty =
+            BindableProperty.Create(
+                nameof(RoundedHorizontally),
+                typeof(bool),
+                typeof(ExtendedButton));
+
+        public bool ActiveGradient { get; set; }
+
+        public Color GradientStartColor { get; set; }
+
+        public Color GradientEndColor { get; set; }
+
+        public bool RoundedHorizontally
+        {
+            get => (bool)GetValue(RoundedHorizontallyProperty);
+            set => SetValue(RoundedHorizontallyProperty, value);
+        }
+
+        protected override void OnPropertyChanged(string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            if (propertyName == HeightProperty.PropertyName)
+            {
+                if (RoundedHorizontally)
+                {
+                    CornerRadius = (int) (Height / 2);
+                }
+            }
+        }
+    }
+}
