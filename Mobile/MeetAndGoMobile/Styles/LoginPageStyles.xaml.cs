882496ac013c6c99030d52edffe7a94724a493ef
Valerii Sovytskyi <valerii.Sovytskyi@jetsoftpro.com>
2018-11-15 18:24:43 +0200

    Added login page styles file

---
+++ Mobile/MeetAndGoMobile/Styles/LoginPageStyles.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MeetAndGoMobile.Styles
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPageStyles : ResourceDictionary
+    {
+		public LoginPageStyles ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
